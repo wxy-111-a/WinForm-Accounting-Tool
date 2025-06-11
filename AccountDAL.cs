14da649a9f3545af462834da1d91dc49507a9918
wxy-111-a <2583551445@qq.com>
2025-06-11 09:31:55 +0800

    Add files via upload

---
+++ AccountDAL.cs
@@ -0,0 +1,105 @@
+﻿using Bookkeeping.Model;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Bookkeeping.DAL
+{
+    /// <summary>
+    /// 资金账户数据访问类
+    /// </summary>
+    public class AccountDAL
+    {
+        /// <summary>
+        /// 获取所有账户信息
+        /// </summary>
+        public List<Account> GetAllAccounts()
+        {
+            List<Account> list = new List<Account>();
+            string sql = "SELECT AccountID, AccountName, Balance FROM Accounts ORDER BY AccountID";
+
+            using (SqlConnection conn = SqlHelper.GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new Account()
+                        {
+                            AccountID = (int)reader["AccountID"],
+                            AccountName = reader["AccountName"].ToString(),
+                            Balance = (decimal)reader["Balance"]
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// 添加一个新账户
+        /// </summary>
+        public int AddAccount(Account account)
+        {
+            string sql = "INSERT INTO Accounts (AccountName, Balance) VALUES (@AccountName, @Balance)";
+            using (SqlConnection conn = SqlHelper.GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@AccountName", account.AccountName);
+                cmd.Parameters.AddWithValue("@Balance", account.Balance);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// 更新一个已有账户
+        /// </summary>
+        public int UpdateAccount(Account account)
+        {
+            string sql = "UPDATE Accounts SET AccountName = @AccountName, Balance = @Balance WHERE AccountID = @AccountID";
+            using (SqlConnection conn = SqlHelper.GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@AccountName", account.AccountName);
+                cmd.Parameters.AddWithValue("@Balance", account.Balance);
+                cmd.Parameters.AddWithValue("@AccountID", account.AccountID);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// 根据ID删除一个账户
+        /// </summary>
+        public int DeleteAccount(int accountId)
+        {
+            // 注意：实际项目中需要检查该账户是否已被交易记录使用
+            string sql = "DELETE FROM Accounts WHERE AccountID = @AccountID";
+            using (SqlConnection conn = SqlHelper.GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@AccountID", accountId);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// (重要) 更新账户余额，使用事务确保数据一致性
+        /// </summary>
+        /// <param name="accountId">账户ID</param>
+        /// <param name="amount">变动金额 (收入为正, 支出为负)</param>
+        /// <param name="transaction">外部传入的数据库事务对象</param>
+        public void UpdateBalance(int accountId, decimal amount, SqlTransaction transaction)
+        {
+            string sql = "UPDATE Accounts SET Balance = Balance + @Amount WHERE AccountID = @AccountID";
+            // 命令需要使用事务关联的连接
+            SqlCommand cmd = new SqlCommand(sql, transaction.Connection, transaction);
+            cmd.Parameters.AddWithValue("@Amount", amount);
+            cmd.Parameters.AddWithValue("@AccountID", accountId);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ UserInfoDAL.cs
@@ -0,0 +1,44 @@
+﻿using Bookkeeping.Model;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Bookkeeping.DAL
+{
+    /// <summary>
+    /// 用户信息数据访问类
+    /// </summary>
+    public class UserInfoDAL
+    {
+        /// <summary>
+        /// 根据用户名和密码获取用户信息
+        /// </summary>
+        /// <param name="userName">用户名</param>
+        /// <param name="password">密码</param>
+        /// <returns>如果找到则返回 UserInfo 对象，否则返回 null</returns>
+        public UserInfo GetUserByLogin(string userName, string password)
+        {
+            UserInfo user = null;
+            string sql = "SELECT UserID, UserName, Password FROM UserInfo WHERE UserName = @UserName AND Password = @Password";
+
+            using (SqlConnection conn = SqlHelper.GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@UserName", userName);
+                cmd.Parameters.AddWithValue("@Password", password); // 明文密码比对
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        user = new UserInfo();
+                        user.UserID = (int)reader["UserID"];
+                        user.UserName = reader["UserName"].ToString();
+                        user.Password = reader["Password"].ToString();
+                    }
+                }
+            }
+            return user;
+        }
+    }
+}
